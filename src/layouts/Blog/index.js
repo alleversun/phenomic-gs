607011893c91e2b8343f4d9ea7d52c2ccf3ad661
alleversun <alleversun@yahoo.com>
2016-12-02 23:12:38 -0500

    missing contact layout, alert network error on blog page

--- src/layouts/Blog/index.js
+++ src/layouts/Blog/index.js
@@ -1,39 +1,14 @@
 import React from "react"
-import axios from "axios"
 
 import LatestPosts from "../../components/LatestPosts"
 import Page from "../Page"
 
-class Blog extends React.Component {
-  constructor(props){
-    super(props);
-
-	this.state = {
-		title: ''
-	};
-
-	axios.get("http://kwsr.godzspeed.com/wp-json/wp/v2/posts/949")
-		.then((response) => {
-			this.setState({title:response.data.title.rendered});
-		}).catch(err => {
-			alert(err.message);
-		});
-  }
-
-  render(){
-
-	let title = 'Loading';
-
-	if(this.state.title){
-		title = this.state.title;
-	}
-
-    return (
-    <Page { ...this.props }>
-      <LatestPosts title={ title } />
+const Blog = (props) => {
+  return (
+    <Page { ...props }>
+      <LatestPosts />
     </Page>
-    )
-  }
+  )
 }
 
 export default Blog
